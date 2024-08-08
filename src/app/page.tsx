14afa127379a1b62295339c306328be1efe9ef0d
GUSTGUSTS <141816627+GUSTGUSTS@users.noreply.github.com>
2024-08-08 15:32:30 +0700

    lastest vers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+
+import PostOwnner from "@/components/PostOwnner";
+import Comment from "@/components/Comment";
+
 export default function Home() {
   return (
     <div
@@ -12,55 +16,16 @@ export default function Home() {
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
-
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
-
+        <PostOwnner text="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"/>
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
+        
+        <Comment
+            userImagePath="/profileImages/lisa.jpg"
+            username="Lisa"
+            commentText="จริงค่า"
+            likeNum= {999}
+            replies= {true}
           />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
 
         {/* Reply Example */}
         <div className="d-flex gap-2 my-2 ps-5">
--- src/components/PostOwnner.tsx
+++ src/components/PostOwnner.tsx
@@ -1,4 +1,29 @@
 "use client";
-export default function PostOwnner({}) {
-  return <div>Your code for PostOwner component ...</div>;
+
+export default function PostOwnner(props:any) {
+  return <div> <div className="vstack gap-3">
+  <div className="d-flex align-items-center gap-3">
+    <img
+      src="/profileImages/Me.jpg"
+      width="48"
+      height="48"
+      className="rounded-circle"
+      style={{ objectFit: "cover" }}
+    />
+    <span className="fw-semibold fs-5 text-white">
+      Kanathip Chiengthong 660612139
+    </span>
+  </div>
+
+  <span className="text-white">
+  {props.text}
+  </span>
+
+  <div className="d-flex align-items-center gap-1">
+    <img src="/like.svg" width={20}></img>
+    <span style={{ color: "#B0B3B8" }}>100 คน</span>
+  </div>
+  <hr className="m-0 border" />
+</div>
+</div>;
 }
